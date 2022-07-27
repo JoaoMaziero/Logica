bf5f9850afcb53a245c734842bcd3a3942ff4b43
João Paulo <jpmaziero@gmail.com>
2022-07-27 11:03:18 -0300

    Incluindo o exercício torneio de natação

--- src/TorneioDeNatacao.java
+++ src/TorneioDeNatacao.java
@@ -3,11 +3,11 @@ public class TorneioDeNatacao {
         String nome = "Luffy";
         int idade = 19;
         if (idade < 11) {
-            System.out.println(nome + " Participará da categoria Infantil");
+            System.out.println(nome + " Participará da categoria Infantil ");
         }else if (idade >= 11 && idade <=15) {
-            System.out.println(nome+" participará da categoria Juvenil");
+            System.out.println(nome+" participará da categoria Juvenil ");
         } else if (idade >= 16 && idade <= 19) {
-            System.out.println(nome+" participará da categoria Pré-Adulto");
+            System.out.println(nome+" participará da categoria Pré-Adulto " );
         } else {;
         }
     }
