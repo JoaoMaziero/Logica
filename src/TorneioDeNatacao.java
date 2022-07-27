d93f4e42392e7d5203b296692b2c002a0a500356
João Paulo <jpmaziero@gmail.com>
2022-07-27 11:01:27 -0300

    Alteração do nome do Projeto

---
+++ src/TorneioDeNatacao.java
@@ -0,0 +1,14 @@
+public class TorneioDeNatacao {
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        int idade = 19;
+        if (idade < 11) {
+            System.out.println(nome + " Participará da categoria Infantil");
+        }else if (idade >= 11 && idade <=15) {
+            System.out.println(nome+" participará da categoria Juvenil");
+        } else if (idade >= 16 && idade <= 19) {
+            System.out.println(nome+" participará da categoria Pré-Adulto");
+        } else {;
+        }
+    }
+}
