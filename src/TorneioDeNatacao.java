4af39d2a8c625ad06866042fcb31eece8d4cafd6
João Paulo <jpmaziero@gmail.com>
2022-07-27 11:38:00 -0300

    Inclusão na classe TorneioDeNatacao leitura de teclado

--- src/TorneioDeNatacao.java
+++ src/TorneioDeNatacao.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class TorneioDeNatacao {
     public static void main(String[] args) {
-        String nome = "Luffy";
-        int idade = 19;
+        Scanner teclado = new Scanner((System.in));
+        System.out.println(" Digite seu nome: ");
+        String nome = teclado.next();
+        System.out.println("Digite sua idade: ");
+        int idade = teclado.nextInt();
         if (idade < 11) {
-            System.out.println(nome + " Participará da categoria Infantil ");
+            System.out.println(nome +" Tem "+ idade +" e  Participará da categoria Infantil ");
         }else if (idade >= 11 && idade <=15) {
-            System.out.println(nome+" participará da categoria Juvenil ");
+            System.out.println(nome +" Tem "+ idade +" e Participará da categoria Juvenil ");
         } else if (idade >= 16 && idade <= 19) {
-            System.out.println(nome+" participará da categoria Pré-Adulto " );
+            System.out.println(nome +" Tem "+ idade +" e Participará da categoria Pré-Adulto " );
         } else {;
         }
     }
