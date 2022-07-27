bdc0157dfad3362ffbe4a70aa4ca694f60ecae4e
João Paulo <jpmaziero@gmail.com>
2022-07-27 11:18:50 -0300

    Exercício que realiza leitura do teclado

---
+++ src/RecebendoDadosDoTeclado.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class RecebendoDadosDoTeclado {
+    public static void main(String[] args) {
+        Scanner  teclado =  new Scanner((System.in));
+        System.out.println("Digite seu nome: ");
+        String nome = teclado.next();
+        System.out.println("Digite sua idade: ");
+        int idade = teclado.nextInt();
+        System.out.println(nome +" Tem " + idade);
+    }
+}
