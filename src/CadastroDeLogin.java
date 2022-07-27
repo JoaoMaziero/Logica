0b34514b0ac93d4f39b6af4813dd103dfb0dadde
João Paulo <jpmaziero@gmail.com>
2022-07-27 11:38:00 -0300

    Inclusão na classe TorneioDeNatacao leitura de teclado

---
+++ src/CadastroDeLogin.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CadastroDeLogin {
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+        System.out.println(("Forneça um nome: "));
+        String nome = imput.next();
+        if (nome.equalsIgnoreCase ("")  || nome.equalsIgnoreCase ( "admin") ||
+                nome.equalsIgnoreCase("administrador")){
+            System.out.println(nome+ " Usuário inválido");
+        }else
+            System.out.println(nome+" Cadastrado com sucesso");
+    }
+
+}
