5b5d4f21852c4281a9b0123a364cb67c0d964679
João Paulo <jpmaziero@gmail.com>
2022-08-02 13:24:03 -0300

    Aulas Java

---
+++ src/academy/devojo/lacosderepeticao/fors/Aula01IntroducaoFors.java
@@ -0,0 +1,11 @@
+package academy.devojo.lacosderepeticao.fors;
+
+public class Aula01IntroducaoFors {
+    public static void main(String[] args) {
+        for (int i = 0; i  <= 10 ; i ++ ) {
+            System.out.println(i);
+        }
+    }
+
+
+}
