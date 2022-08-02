cee285a1a94ce615e7692a01287aed47d491d310
João Paulo <jpmaziero@gmail.com>
2022-08-02 12:21:20 -0300

    Aula04ExercicioLogin

---
+++ src/academy/devojo/lacosderepeticao/fors/Aula02Exercicio02.java
@@ -0,0 +1,9 @@
+package academy.devojo.lacosderepeticao.fors;
+
+public class Aula02Exercicio02 {
+    public static void main(String[] args) {
+        for (int i = 0; i<=10; i++ ) {
+            System.out.println(5*i);
+        }
+    }
+}
